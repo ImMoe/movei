738811f8be754207da61002a6dc8a47d08f8bb2f
= <immoe98@gmail.com>
2020-08-17 22:24:13 +0200

    Finishing up

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
 const movies = document.querySelectorAll('.movie video')
+const body = document.querySelector('body')
 
 movies.forEach((movie) => {
+  movie.play()
   movie.addEventListener('mouseover', playVideo)
   movie.addEventListener('mouseout', stopVideo)
+})
+
+body.addEventListener('mouseover', () => {
+  movies.forEach((movie) => {
+    movie.mute = true
+  })
 })
 
 function playVideo(e) {
   setTimeout(() => {
     e.target.muted = false
+    e.target.autoplay = true
     e.target.play()
   }, 800)
 }
 
 function stopVideo(e) {
-  setTimeout(() => {
-    e.target.muted = true
-    e.target.load()
-    e.target.pause()
-  }, 800)
+  e.target.muted = true
 }
